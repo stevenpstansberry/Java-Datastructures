b9c178fc6afad055d924a0e19c6fd79551b0b086
Steven <stevenpstansberry@gmail.com>
2022-12-12 11:50:15 -0800

    Exercise_06_01 added

---
+++ Chapter 6 Methods/src/Exercise_06_01.java
@@ -0,0 +1,12 @@
+public class Exercise_06_01 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 101; i++){
+            System.out.print(getPentagonalNumber(i) + " ");
+            if (i % 10 == 0)
+                System.out.println();
+        }
+    }
+    public static int getPentagonalNumber(int n){
+            return (n * (3 *n -1) / 2);
+        }
+    }
